e8cc38ea67d33b97873693f4f91456326de024bb
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2022-03-27 06:34:01 -0300

    criação da pagina new room e integração de paginas

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,20 +1,11 @@
-import { useNavigate } from 'react-router-dom';
-
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg' ;
 
 import '../styles/auth.scss';
 
 import { Button } from '../components/Button';
 
-export function Home() {
-  const navigate = useNavigate();
-
-  function navigationToNewRoom() {
-    navigate('/rooms/new');
-  }
-
+export function NewRoom() {
   return (
     <div id="page-auth">
       <aside>
@@ -25,20 +16,19 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={navigationToNewRoom} className="create-room">
-            <img src={googleIconImg} alt=" logo do Google" />
-            Crie sua sala com o Google
-          </button>
-          <div className="separator"> ou entre na sala</div>
+          <h2>Criar uma nova sala</h2>
           <form>
             <input 
               type="text" 
-              placeholder="Digite o código da sala"
+              placeholder="Nome da sala"
             />
             <Button type="submit">
-              Entrar na sala
+              Criar sala
             </Button>
           </form>
+          <p>
+            Quer entrar em uma sala existente? <a href="#">Clique aqui</a> 
+          </p>
         </div>
       </main>
     </div>
