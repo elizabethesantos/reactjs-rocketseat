d091d1fcf301f65f5bdf054668f6153ffb5ede8b
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2022-03-30 03:44:47 -0300

    Update Home.tsx

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,23 +1,21 @@
-import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg' ;
 
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function Home() {
   const navigate = useNavigate();
-  const { user, signInWithGoogle } = useContext(AuthContext)
+  const { user, signInWithGoogle } = useAuth()
   
-  function handleCreateRoom() {
+  async function handleCreateRoom() {
     if (!user) {
-      signInWithGoogle()
+      await signInWithGoogle()
     }
 
     navigate('/rooms/new');      
