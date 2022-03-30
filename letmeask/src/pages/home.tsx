b73339b323cef84b7ded81f85cf598dd137d4d29
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2022-03-30 08:28:32 -0300

    criação pagina new room

--- letmeask/src/pages/home.tsx
+++ letmeask/src/pages/home.tsx
@@ -1,8 +1,11 @@
 import { useNavigate } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg' ;
+
+import {database} from '../services/firebase';
 
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
@@ -12,13 +15,31 @@ import '../styles/auth.scss';
 export function Home() {
   const navigate = useNavigate();
   const { user, signInWithGoogle } = useAuth()
-  
+  const [roomCode, setRoomCode] = useState('');
+
   async function handleCreateRoom() {
     if (!user) {
       await signInWithGoogle()
     }
 
     navigate('/rooms/new');      
+  }
+
+  async function handleJoinRoom(event: FormEvent){
+    event.preventDefault();
+
+    if (roomCode.trim() ==='') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return;
+    }
+
+    navigate(`/rooms/${roomCode}`);
   }
 
   return (
@@ -35,11 +56,13 @@ export function Home() {
             <img src={googleIconImg} alt=" logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div className="separator"> ou entre na sala</div>
-          <form>
+          <div className="separator"> ou entre em  uma sala</div>
+          <form onSubmit={handleJoinRoom}>
             <input 
               type="text" 
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
