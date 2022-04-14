8bcdcd1661b4176b910e05ee8bd3bcea0d33ae7a
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2022-04-14 07:19:49 -0300

    aula 4

--- letmeask/src/components/Button.tsx
+++ letmeask/src/components/Button.tsx
@@ -9,6 +9,8 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 export function Button({isOutlined = false, ...props}: ButtonProps) {
   return( 
     <button
-     className={`button ${isOutlined ? 'outlined' : ''}`} {...props} />
+     className={`button ${isOutlined ? 'outlined' : ''}`}
+     {...props}
+     />
   )
 }
