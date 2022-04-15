11d4574f1d20fc76a9fcc52946b5725e02838e02
Elizabeth dos Santos <elizabethsantos2402@gmail.com>
2022-04-15 03:47:51 -0300

    s

--- letmeask/src/components/Question/index.tsx
+++ letmeask/src/components/Question/index.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import cx from 'classnames';
 
 import './styles.scss';
 
@@ -7,17 +8,27 @@ type QuestionProps = {
   author: {
     name: string;
     avatar: string;
-  }
+  };
   children?: ReactNode;  
+  isAnswered?:boolean;
+  isHighlighted?:boolean;
 }
 
 export function Question( {
   content,
   author,
+  isAnswered = false,
+  isHighlighted = false,
   children,
 }:QuestionProps){
   return (
-    <div className="question">
+    <div
+      className={cx(
+       'question',
+       {isAnswered: isAnswered},
+       { highlighted: isHighlighted && !isAnswered },
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
